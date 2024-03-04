102004cf1095b8dc9900632fc8d4032f06be5369
Davi Costa <davim.costa@outlook.com>
2024-03-04 19:06:01 -0300

    criação do decorator para converter o id do usuário

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,4 +1,7 @@
 import { Controller, Post, UsePipes, ValidationPipe, Param, Body } from '@nestjs/common';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { UserType } from 'src/user/enum/userType.enum';
 import { AddressService } from './address.service';
 import { CreateAddressDTO } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
@@ -10,13 +13,15 @@ export class AddressController {
         private readonly addressService: AddressService
     ){}
 
-    @Post('/:userId') 
+    @Roles(UserType.User)
+    @Post() 
     @UsePipes(ValidationPipe)
     async createAddress(
         @Body()
         createAddressDTO: CreateAddressDTO,
-        @Param('userId') userId: number
+        @UserId() userId: number
         ): Promise<AddressEntity> {
+            console.log(userId)
         return this.addressService.createAddress(createAddressDTO, userId)
     }
 }
